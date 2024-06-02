6f03d82160c68abf759b37074cdadc1ba4093fa9
Serhii <xx.code4u@gmail.com>
2024-06-02 17:06:51 +0300

    Buttons
    Change button style in desktop mode

--- src/components/cardAnimation/CardAnimation.jsx
+++ src/components/cardAnimation/CardAnimation.jsx
@@ -1,74 +1,168 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './CardAnimation.module.css';
+import Loader from 'components/loader/Loader';
+import axios from 'axios'; // Імпорт axios для HTTP-запитів
 
 const CardAnimation = () => {
+  const [campers, setCampers] = useState([]);
+  const [loading, setLoading] = useState(true); // Встановіть початковий стан завантаження на true
+
+  useEffect(() => {
+    const fetchCampers = async () => {
+      try {
+        const response = await axios.get(
+          'https://6632bb43f7d50bbd9b473f15.mockapi.io/advert'
+        );
+        setCampers(response.data.slice(0, 4));
+        setLoading(false); // Встановіть loading на false, коли дані завантажено
+      } catch (error) {
+        console.error('Error fetching campers:', error);
+        setLoading(false); // Встановіть loading на false в разі помилки
+      }
+    };
+
+    fetchCampers();
+  }, []);
+
   return (
-    <div className={styles.module}>
-      <div className={styles.header}>
-        <h2 className={styles.title}>
-          Most popular <br /> campers location
-        </h2>
-      </div>
-      <div className={styles.wrapper}>
-        <div className={styles.container}>
-          <input
-            className={styles.input}
-            type="radio"
-            name="slide"
-            id="c1"
-            defaultChecked
-          />
-          <label htmlFor="c1" className={styles.card}>
-            <div className={styles.row}>
-              <div className={styles.icon}>1</div>
-              <div className={styles.description}>
-                <h4 className={styles.h4}>Winter</h4>
-                <p className={styles.p}>
-                  Winter has so much to offer - creative and exciting
-                </p>
-              </div>
+    <>
+      {loading ? (
+        <Loader />
+      ) : (
+        <div className={styles.module}>
+          <div className={styles.header}>
+            <h2 className={styles.title}>Most popular campers</h2>
+          </div>
+          <div className={styles.wrapper}>
+            <div className={styles.container}>
+              {campers.map((camper, index) => (
+                <label
+                  key={index}
+                  htmlFor={`c${index + 1}`}
+                  className={styles.card}
+                  style={{
+                    backgroundImage: `url(${
+                      camper.gallery && camper.gallery[0]
+                    })`,
+                  }}
+                >
+                  <input
+                    className={styles.input}
+                    type="radio"
+                    name="slide"
+                    id={`c${index + 1}`}
+                    defaultChecked={index === 0} // Перевірте, чи це перший елемент
+                  />
+                  <div className={styles.row}>
+                    <div className={styles.icon}>{index + 1}</div>
+                    <div className={styles.description}>
+                      <h4 className={styles.h4}>{camper.name}</h4>
+                      <p className={styles.p}>{camper.description}</p>
+                    </div>
+                  </div>
+                </label>
+              ))}
             </div>
-          </label>
-          <input className={styles.input} type="radio" name="slide" id="c2" />
-          <label htmlFor="c2" className={styles.card}>
-            <div className={styles.row}>
-              <div className={styles.icon}>2</div>
-              <div className={styles.description}>
-                <h4 className={styles.h4}>Summer</h4>
-                <p className={styles.p}>
-                  Summer has so much to offer - creative and exciting
-                </p>
-              </div>
-            </div>
-          </label>
-          <input className={styles.input} type="radio" name="slide" id="c3" />
-          <label htmlFor="c3" className={styles.card}>
-            <div className={styles.row}>
-              <div className={styles.icon}>3</div>
-              <div className={styles.description}>
-                <h4 className={styles.h4}>Fall</h4>
-                <p className={styles.p}>
-                  Fall has so much to offer - creative and exciting
-                </p>
-              </div>
-            </div>
-          </label>
-          <input className={styles.input} type="radio" name="slide" id="c4" />
-          <label htmlFor="c4" className={styles.card}>
-            <div className={styles.row}>
-              <div className={styles.icon}>4</div>
-              <div className={styles.description}>
-                <h4 className={styles.h4}>Spring</h4>
-                <p className={styles.p}>
-                  Spring has so much to offer - creative and exciting
-                </p>
-              </div>
-            </div>
-          </label>
+          </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
 export default CardAnimation;
+
+// import Loader from 'components/loader/Loader';
+
+// const CardAnimation = () => {
+//   // const [loading, setLoading] = useState(false);
+//   // useEffect(() => {
+//   //   const fetchCampers = async () => {
+//   //     try {
+//   //       const response = await axios.get(
+//   //         'https://6632bb43f7d50bbd9b473f15.mockapi.io/advert'
+//   //       );
+//   //       setCampers(response.data);
+//   //       setLoading(false);
+//   //     } catch (error) {
+//   //       console.error('Error fetching campers:', error);
+//   //       setLoading(false);
+//   //     }
+//   //   };
+
+//   //   fetchCampers();
+//   // }, []);
+
+//   return (
+//     <>
+//       <div className={styles.module}>
+//         <div className={styles.header}>
+//           <h2 className={styles.title}>
+//             Most popular <br /> campers location
+//           </h2>
+//         </div>
+//         <div className={styles.wrapper}>
+//           <div className={styles.container}>
+//             <input
+//               className={styles.input}
+//               type="radio"
+//               name="slide"
+//               id="c1"
+//               defaultChecked
+//             />
+//             <label htmlFor="c1" className={styles.card}>
+//               <div className={styles.row}>
+//                 <div className={styles.icon}>1</div>
+//                 <div className={styles.description}>
+//                   <h4 className={styles.h4}>Winter</h4>
+//                   <p className={styles.p}>
+//                     Winter has so much to offer - creative and exciting
+//                   </p>
+//                 </div>
+//               </div>
+//             </label>
+//             <input className={styles.input} type="radio" name="slide" id="c2" />
+//             <label htmlFor="c2" className={styles.card}>
+//               <div className={styles.row}>
+//                 <div className={styles.icon}>2</div>
+//                 <div className={styles.description}>
+//                   <h4 className={styles.h4}>Summer</h4>
+//                   <p className={styles.p}>
+//                     Summer has so much to offer - creative and exciting
+//                   </p>
+//                 </div>
+//               </div>
+//             </label>
+//             <input className={styles.input} type="radio" name="slide" id="c3" />
+//             <label htmlFor="c3" className={styles.card}>
+//               <div className={styles.row}>
+//                 <div className={styles.icon}>3</div>
+//                 <div className={styles.description}>
+//                   <h4 className={styles.h4}>Fall</h4>
+//                   <p className={styles.p}>
+//                     Fall has so much to offer - creative and exciting
+//                   </p>
+//                 </div>
+//               </div>
+//             </label>
+//             <input className={styles.input} type="radio" name="slide" id="c4" />
+//             <label htmlFor="c4" className={styles.card}>
+//               <div className={styles.row}>
+//                 <div className={styles.icon}>4</div>
+//                 <div className={styles.description}>
+//                   <h4 className={styles.h4}>Spring</h4>
+//                   <p className={styles.p}>
+//                     Spring has so much to offer - creative and exciting
+//                   </p>
+//                 </div>
+//               </div>
+//             </label>
+//           </div>
+//         </div>
+//       </div>
+//       )}
+//     </>
+//   );
+// };
+
+// export default CardAnimation;
--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -45,6 +45,7 @@ const Navigation = () => {
         {windowWidth >= 768 ? (
           <div className={css.navbarLinksDesktop}>
             <NavLink
+              className={css.LinksDesktop}
               to="/"
               onClick={closeMobileMenu}
               activeClassName={css.active}
@@ -52,6 +53,7 @@ const Navigation = () => {
               Home
             </NavLink>
             <NavLink
+              className={css.LinksDesktop}
               to="/catalog"
               onClick={closeMobileMenu}
               activeClassName={css.active}
@@ -59,6 +61,7 @@ const Navigation = () => {
               Catalog
             </NavLink>
             <NavLink
+              className={css.LinksDesktop}
               to="/favorites"
               onClick={closeMobileMenu}
               activeClassName={css.active}
@@ -81,76 +84,3 @@ const Navigation = () => {
 };
 
 export default Navigation;
-
-// import React, { useState, useEffect } from 'react';
-// import { NavLink } from 'react-router-dom';
-// import css from './Navigation.module.css';
-// import { FaBars } from 'react-icons/fa';
-// // import Logo from '../images/sprite/icons.svg#icon-logo1';
-// import MobileMenuModal from '../modals/mobileMenuModal/MobileMenuModal';
-// import Logo from 'components/logo/Logo';
-
-// const Navigation = () => {
-//   const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
-//   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
-
-//   const toggleMobileMenu = () => {
-//     setMobileMenuOpen(!isMobileMenuOpen);
-//   };
-
-//   const closeMobileMenu = () => {
-//     setMobileMenuOpen(false);
-//   };
-
-//   useEffect(() => {
-//     const handleResize = () => {
-//       setWindowWidth(window.innerWidth);
-//       if (window.innerWidth >= 768) {
-//         setMobileMenuOpen(false);
-//       }
-//     };
-
-//     window.addEventListener('resize', handleResize);
-//     return () => {
-//       window.removeEventListener('resize', handleResize);
-//     };
-//   }, []);
-
-//   return (
-//     <div className={css.navigation}>
-//       <nav className={css.navBlock}>
-//         <div className={css.navbarLogo}>
-//           <NavLink to="/">
-//             <Logo />
-//           </NavLink>
-//         </div>
-//         <div className={css.division}>
-//           <div className={css.logoDivision}>
-//             <h1 className={css.logoTitle}>NOMAD</h1>
-//             <h3 className={css.logoText}>Campers rental</h3>
-//           </div>
-//         </div>
-//         {windowWidth >= 768 ? (
-//           <div className={css.navbarLinksDesktop}>
-//             <NavLink to="/" onClick={closeMobileMenu}>
-//               Home
-//             </NavLink>
-//             <NavLink to="/catalog" onClick={closeMobileMenu}>
-//               Catalog
-//             </NavLink>
-//             <NavLink to="/favorites" onClick={closeMobileMenu}>
-//               Favorites
-//             </NavLink>
-//           </div>
-//         ) : (
-//           <div className={css.navbarMobileMenu} onClick={toggleMobileMenu}>
-//             <FaBars />
-//           </div>
-//         )}
-//         <MobileMenuModal isOpen={isMobileMenuOpen} onClose={closeMobileMenu} />
-//       </nav>
-//     </div>
-//   );
-// };
-
-// export default Navigation;
